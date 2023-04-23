048679099e17a6c63ff458c622e3ed6b1e31c192
saul goodman <sgwszzh@gmail.com>
2023-04-23 09:43:46 +0800

    nextjs blog demo

---
+++ pages/index.js
@@ -0,0 +1,47 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Link from 'next/link'
+import Layout, { siteTitle } from '@/components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '@/lib/posts'
+import Date from '@/components/date'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello, I’m Shu. I’m a software engineer and a translator (English/Japanese). You can contact me on Twitter.
+
+          (This is a sample website - you’ll be building a site like this in our Next.js tutorial.)</p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
---
+++ pages/posts/first-post.js
@@ -0,0 +1,17 @@
+import Layout from "@/components/layout";
+import Head from "next/head";
+import Link from "next/link";
+
+export default function FirstPost() {
+    return (
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">Back to home</Link>
+            </h2>
+        </Layout>
+    );
+}
